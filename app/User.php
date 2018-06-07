8cc41ccf949eab3f69ae4c6c912545eb1a05d238
daiki.ishibashi <daiki.a.ishibashi@rakuten.com>
2018-06-07 02:56:48 +0000

    Procfile

--- app/User.php
+++ app/User.php
@@ -29,9 +29,7 @@ class User extends Authenticatable
    
     public function microposts()
     {
-        $follow_user_ids = $this->followings()-> pluck('users.id')->toArray();
-        $follow_user_ids[] = $this->id;
-        return Micropost::whereIn('user_id', $follow_user_ids);
+       return $this->hasMany(Micropost::class);
     }
     
     public function followings()
@@ -83,6 +81,12 @@ public function unfollow($userId)
         return $this->followings()->where('follow_id', $userId)->exists();
     }
 
+    public function feed_microposts()
+    {
+        $follow_user_ids = $this->followings()-> pluck('users.id')->toArray();
+        $follow_user_ids[] = $this->id;
+        return Micropost::whereIn('user_id', $follow_user_ids);
+    }
 }
 
 
